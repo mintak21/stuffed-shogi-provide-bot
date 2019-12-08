742c0a374b447ce9d36573c640c093e1c4d95fe1
mintak-Macbook <mintak21@gmail.com>
2019-12-08 21:13:21 +0900

    fix miss

--- config/gunicorn_conf.py
+++ config/gunicorn_conf.py
@@ -1,20 +1,17 @@
 import os
 
-if False:
-    # Sever Socket
-    host = '0.0.0.0'  # Flaskは外部公開を許していないので、基本これ
-    port = os.getenv('PORT', 5000)
+# Sever Socket
+host = '0.0.0.0'  # Flaskは外部公開を許していないので、基本これ
+port = os.getenv('PORT', 5000)
 
-    # bind = str(host) + ':' + str(port)
-    bind = 'unix:/tmp/stuffed-shogi-provider-bot.socket'
+# bind = str(host) + ':' + str(port)
+bind = 'unix:/tmp/stuffed-shogi-provider-bot.socket'
 
-    # Debugging
-    reload = True
+# Debugging
+reload = True
 
-    # Logging
-    accesslog = '-'
+# Logging
+accesslog = '-'
 
-    # Proc Name
-    proc_name = 'Tsume-Shogi Provider'
-else:
-    bind = '127.0.0.1:8090'
+# Proc Name
+proc_name = 'Tsume-Shogi Provider'
